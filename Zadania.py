90af0ac36a099de6ff5d182864b095f377eca199
Donpeq <mateusz.lusowko@gmail.com>
2022-02-19 23:51:27 +0100

    My project first commit

---
+++ Zadania.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    print("Obliczam pole prostokąta")
+    a = float(input("podaj krótszy bok w cm:",))
+    print()
+    b = float(input("podaj dłuższy bok w cm:",))
+    print()
+    prost = a * b
+    print("pole prostokąta wynosi:", prost, "cm")
